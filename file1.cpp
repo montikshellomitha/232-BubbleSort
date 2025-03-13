08d87dc72f8a551577dd5bfb1f96f8b0e03314ef
montikshellomitha <monshelmit@gmail.com>
2025-03-13 11:26:42 +0700

    menambahkan display

--- file1.cpp
+++ file1.cpp
@@ -43,6 +43,25 @@ void bubbleSortArray(){
     }while(pass <= n-1);  //Step 5
 }
 
+void display(){
+     cout << "================================" << endl;
+     cout << "Elemen Array yang telah tersusun" << endl;
+     cout << "================================" << endl;
+     cout << endl;
+     for (int j= 0;j<n;j++){
+         cout << arr[j];
+         if (j<n-1){
+             cout << "-->>";
+         }
+     }
+}
+
 int main(){
-    
+    input();
+    bubbleSortArray();
+    display();
+}
+
+int main(){
+
 }
